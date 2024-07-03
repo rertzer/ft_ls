69098fc70b855d3e74ab05ee93ce3772630297c5
rertzer <rertzer@student.42.fr>
2024-07-03 16:04:18 +0200

    reformating bonus strcmp

--- src/bonus_strcmp.c
+++ src/bonus_strcmp.c
@@ -14,102 +14,96 @@
 #include "ft_ls.h"
 
 static inline size_t	skip_dots(const char **s);
+static int case_insensitive_alphanum_compare(const char *s1, const char *s2, bool *isalnum);
+static int	alphanum_compare(const char *s1, const char *s2);
 
 int	ls_strcmp(const char *s1, const char *s2)
 {
   	int	diff = 0;
+	bool	isalnum = false;
 
 	size_t	dots_1 = skip_dots(&s1);
 	size_t	dots_2 = skip_dots(&s2);
-	char	a = 0;
-	char	b = 0;
+
+	diff = case_insensitive_alphanum_compare(s1, s2, &isalnum);
+	if (isalnum)
+	{
+		if (diff == 0)
+		{
+			diff = alphanum_compare(s1, s2);	
+			if (diff == 0)
+			{
+				diff = ft_strcmp(s1, s2);
+				if (diff == 0)
+				{
+					diff = dots_2 - dots_1;
+				}
+			}
+		}
+	}
+	else if (diff == 0)
+	{
+		diff = dots_1 - dots_2;
+	}
+
+	return (diff);
+}
+
+static int case_insensitive_alphanum_compare(const char *s1, const char *s2, bool *isalnum)
+{
 	size_t	i = 0;
 	size_t	j = 0;
-	bool	isalnum = false;
 
-		a = ft_toupper(s1[i]);
-		b = ft_toupper(s2[j]);
 	while (s1[i] != '\0' && s2[j] != '\0')
 	{ 
 		if (! ft_isalnum(s1[i]))
 		{
 			++i;
-			a = ft_toupper(s1[i]);
 			continue;
 		}
 		if (! ft_isalnum(s2[j]))
 		{
 			++j;
-			b = ft_toupper(s2[j]);
 			continue;
 		}
-		isalnum = true;
+		*isalnum = true;
 		
-		if (a != b)
-		  break;
+		if (ft_toupper(s1[i]) != ft_toupper(s2[j]))
+		{
+			break;
+		}
 		++i;
 		++j;
-		a = ft_toupper(s1[i]);
-		b = ft_toupper(s2[j]);
 	}
-	if (isalnum)
-	{
-		if (s1[i] == '\0' && s2[j] == '\0')
-		{
-			i = 0;
-			j = 0;
-			while (s1[i] != '\0' && s2[j] != '\0')
-			{
-				if (! ft_isalnum(s1[i]))
-				{
-					++i;
-					continue;
-				}
-				if (! ft_isalnum(s2[j]))
-				{
-					++j;
-					continue;
-				}
-
-				if (s1[i] != s2[j])
-					break;
-				++i;
-				++j;
-			}
-			diff = s2[j] - s1[i];
-			if (diff == 0)
-			{
-				i = 0;
-				j = 0;
-				while (s1[i] != '\0' && s2[j] != '\0')
-				{
-					
-					if (s1[i] != s2[j])
-						break;
-					++i;
-					++j;
-				}
-				diff = (unsigned int)s1[i] - (unsigned int)s2[j];
-			}
-		}
-		else
-		{
-			diff = (unsigned char)a - (unsigned char)b;
-		}
-		if (diff == 0)
-		{
-			diff = dots_2 - dots_1;
-		}
-	}
-	else
-		{
-			if (a == '\0' && b == '\0')
-				diff = dots_1 - dots_2;
-			else
-				diff = (unsigned char)a - (unsigned char)b;
-		}
-	return (diff);
+	return ((unsigned char)ft_toupper(s1[i]) - (unsigned char)ft_toupper(s2[j]));
 }
+
+static int	alphanum_compare(const char *s1, const char *s2)
+{
+	size_t	i = 0;
+	size_t	j = 0;
+
+	while (s1[i] != '\0' && s2[j] != '\0')
+	{
+		if (! ft_isalnum(s1[i]))
+		{
+			++i;
+			continue;
+		}
+		if (! ft_isalnum(s2[j]))
+		{
+			++j;
+			continue;
+		}
+
+		if (s1[i] != s2[j])
+			break;
+		++i;
+		++j;
+	}
+	return ((unsigned int)s2[j] - (unsigned int)s1[i]);
+}
+
 
 static inline size_t	skip_dots(const char **s)
 {
